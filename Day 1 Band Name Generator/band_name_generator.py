e7cd2ed78ad329d992fcdcab5334834706eb86bb
Daniel Z <43827694+dzhuge@users.noreply.github.com>
2021-04-28 16:42:02 -0700

    Delete Day 1 Band Name Generator directory

--- Day 1 Band Name Generator/band_name_generator.py
+++
@@ -1,9 +0,0 @@
-# Day 1 of 100 Days of Code
-# This program will ask user for birth place and their pet name, and generate a band name based on input
-# Topics covered: print, input, basic string manipulation
-def generate_band_name():
-    city_name = input("What's the name of the city you grew up in? ")
-    pet_name = input("What's your pet's name? ")
-    print(f"Your band name could be: {city_name} {pet_name}")
-
-generate_band_name()
