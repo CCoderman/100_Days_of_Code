3208522021fb27febc45459560feabf5ee74a9a1
Jawn Smiff <43827694+dzhuge@users.noreply.github.com>
2021-04-28 17:36:50 -0700

    Add files via upload

--- Day 1 Band Name Generator/band_name_generator.py
+++ Day 1 Band Name Generator/band_name_generator.py
@@ -2,8 +2,11 @@
 # This program will ask user for birth place and their pet name, and generate a band name based on input
 # Topics covered: print, input, basic string manipulation
 def generate_band_name():
-    city_name = input("What's the name of the city you grew up in? ")
-    pet_name = input("What's your pet's name? ")
+    # Ask the user for the city they grew up in
+    city_name = input("What's the name of the city you grew up in?\n")
+    # Ask the user for the name of their pet
+    pet_name = input("What's your pet's name?\n")
+    # Generates the band name (city name + pet name)
     print(f"Your band name could be: {city_name} {pet_name}")
 
 
